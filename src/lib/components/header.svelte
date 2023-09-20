4438c370b8410bc2569a89be69118ad5d8539d37
penguin dev <74113025+polish-penguin-dev@users.noreply.github.com>
2023-09-20 19:48:23 +0100

    Add basic header

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,14 +1,43 @@
 <div class="header">
+    <input type="text" class="main" placeholder="Search">
+    <div class="menu"><img src="/menu.png" alt="menu" /></div>
+ </div>
+ 
+ <style lang="scss">
+     .header {
+         display: flex;
+         align-items: center;
+         width: calc(100% - 24px);
+         margin: 12px;
+     }
+     
+     .header > .main {
+         background-color: #94d2bd;
+         color: #ffffff;
+         height: 48px;
+         flex-grow: 1; 
+         margin-right: 8px; 
+         border-radius: 0.25rem;
+         border: none;
+         padding: 0 12px;
+         outline: none;
+     }
+ 
+     .header > .menu {
+         display: flex;
+         align-items: center;
+         justify-content: center;
+         background-color: #94d2bd;
+         height: 48px;
+         width: 48px;
+         border-radius: 0.25rem;
+         font-size: 32px;
+         cursor: pointer;
+     }
 
-</div>
-
-<style type="scss">
-    .header {
-        background-color: #aaf0d1;
-        width: 100%;
-        height: 48px;
-        margin: 8px;
-        border-radius: 8px;
-        box-sizing: border-box;
-    }
-</style>
+     .header > .menu > img {
+        width: 32px;
+        height: 32px;
+     }
+ </style>
+ 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,11 @@
-<style global>
-    body {
-        background-color: #191919;
+<style>
+    :global(body) {
+        background-color: #001219;
         width: 100%;
         height: 100%;
         margin: 0;
         padding: 0;
-        box-sizing: border-box;
     }
 </style>
-  
+
 <slot />
-  
