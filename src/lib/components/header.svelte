de84b0ca01d794cbf65225ec3439b494953fe6e1
penguin dev <74113025+polish-penguin-dev@users.noreply.github.com>
2023-09-20 17:23:02 +0100

    Basic Setup

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,14 @@
+<div class="header">
+
+</div>
+
+<style type="scss">
+    .header {
+        background-color: #aaf0d1;
+        width: 100%;
+        height: 48px;
+        margin: 8px;
+        border-radius: 8px;
+        box-sizing: border-box;
+    }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<style global>
+    body {
+        background-color: #191919;
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+</style>
+  
+<slot />
+  
